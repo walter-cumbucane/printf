33ab354604c428da4258a5507edf46cb30a870b8
Wairimu5 <innonjue@gmail.com>
2023-07-25 03:26:49 -0700

    update

--- _printnums.c
+++ _printnums.c
@@ -1,109 +1,89 @@
-#include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 /**
- * _putchar - Helper function to write a single character to stdout.
- * @c: The character to be written.
- *
- * Return: On success, returns the number of characters written.
- *         On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return write(1, &c, 1);
-}
-
-/**
- * _printf - Produces output according to a format.
- * @format: The format string.
+ * _printint - Helper function to print an integer directly to stdout.
+ * @num: The integer to be printed.
  *
  * Return: The number of characters printed (excluding the null byte).
- *         On error, -1 is returned.
  */
-int _printf(const char *format, ...)
+int _printint(int num)
 {
-	va_list args;
+	char buffer[20]; /*Assume that 20 charaters are enough for an int*/
+	int count = 0;
+	int i = sizeof(buffer) - 1;
+
+	if (num == 0)/*Handle Zero */
+	{
+		buffer[0] = 48;
+		buffer[1] = 0;
+		count++;
+		write(1, buffer, 1);
+		return (count);
+	}
+	if (num < 0)/*Handle Negative numbers */
+	{
+		write(1, "-", 1);
+		count++;
+		num = -num;
+	}
+	buffer[i--] = 0;/*Converts the integer to a string(reversed)*/
+	do {
+		buffer[i--] = 48 + (num % 10);
+		num /= 10;
+		count++;
+	} while (num > 0);
+	write(1, buffer + i + 1, count);/*Write the string to the stdout */
+	return (count);
+}
+
+/**
+ * _printbin - Helper function to print a binary directly to stdout.
+ * @num: The integer to be printed in binary
+ *
+ * Return: The number of characters printed (excluding the null byte).
+ */
+
+int _printbin(int num)
+{
+	int num_bits = sizeof(num) * 8;
+	char *binary;
+	int i;
 	int count = 0;
 
-	va_start(args, format);
-
-	while (*format)
+	if (num < 0)/*Handle negative numbers */
+		return (0);
+	if (num == 0)/* Handle zero */
 	{
-		if (*format == '%')
-		{
-			format++;
-
-			switch (*format)
-			{
-			case 'c':
-			{
-				char ch = va_arg(args, int);
-				count += _putchar(ch);
-				break;
-			}
-			case 's':
-			{
-				char *str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(null)";
-				while (*str)
-				{
-					count += _putchar(*str);
-					str++;
-				}
-				break;
-			}
-			case '%':
-				count += _putchar('%');
-				break;
-			case 'd':
-			case 'i':
-			{
-				int num = va_arg(args, int);
-				int temp = num;
-				int digits = 0;
-
-				if (num == 0)
-				{
-					count += _putchar('0');
-					break;
-				}
-
-				if (num < 0)
-				{
-					count += _putchar('-');
-					num = -num;
-				}
-
-				while (temp != 0)
-				{
-					digits++;
-					temp /= 10;
-				}
-
-				while (digits--)
-				{
-					int digit = num / pow(10, digits);
-					count += _putchar(digit + '0');
-					num %= (int)pow(10, digits);
-				}
-
-				break;
-			}
-			default:
-				count += _putchar('%');
-				count += _putchar(*format);
-			}
-		}
-		else
-		{
-			count += _putchar(*format);
-		}
-
-		format++;
+		write(1, "0", 1);
+		return (1);
 	}
-
-	va_end(args);
-
-	return count;
+	binary = malloc((num_bits + 1) * sizeof(char));
+	for (i = num_bits - 1; i >= 0; i--)/*loop through each bit of num*/
+	{
+		if (num & (1 << i))/*Determines whether the bit "i" is 1 or 0*/
+			*(binary + num_bits - i - 1) = '1';
+		else
+			*(binary + num_bits - i - 1) = '0';
+	}
+	*(binary + num_bits) = '\0';
+	i = 0;
+	while (*(binary + i) != 0)
+	{
+		while (*(binary + i) != '1')
+		{
+			i++;
+			continue;
+		}
+		break;
+	}
+	while (*(binary + i) != 0)
+	{
+		write(1, binary + i, 1);
+		count++;
+		i++;
+	}
+	free(binary);
+	return (count);
 }
+
