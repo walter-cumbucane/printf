091bcd5dfe3b26a42fba51ae65e0bcc92e07bce6
Wairimu5 <innonjue@gmail.com>
2023-07-25 10:28:06 -0700

    update

--- _printnums.c
+++ _printnums.c
@@ -1,5 +1,17 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * _putchar - Helper function to print a single character to stdout.
+ * @c: The character to be printed.
+ *
+ * Return: 1 on success, -1 on error.
+ */
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
 
 /**
  * _printint - Helper function to print an integer directly to stdout.
@@ -9,31 +21,82 @@
  */
 int _printint(int num)
 {
-	char buffer[20]; /*Assume that 20 charaters are enough for an int*/
+	char buffer[20]; /* Assume that 20 characters are enough for an int */
 	int count = 0;
 	int i = sizeof(buffer) - 1;
 
-	if (num == 0)/*Handle Zero */
+	if (num == 0)/* Handle Zero */
 	{
-		buffer[0] = 48;
-		buffer[1] = 0;
+		buffer[0] = '0';
+		buffer[1] = '\0';
 		count++;
-		write(1, buffer, 1);
+		_putchar('0');
 		return (count);
 	}
-	if (num < 0)/*Handle Negative numbers */
+	if (num < 0)/* Handle Negative numbers */
 	{
-		write(1, "-", 1);
+		_putchar('-');
 		count++;
 		num = -num;
 	}
-	buffer[i--] = 0;/*Converts the integer to a string(reversed)*/
+	buffer[i--] = '\0';/* Converts the integer to a string (reversed) */
 	do {
-		buffer[i--] = 48 + (num % 10);
+		buffer[i--] = '0' + (num % 10);
 		num /= 10;
 		count++;
 	} while (num > 0);
-	write(1, buffer + i + 1, count);/*Write the string to the stdout */
+	_putchar(buffer[i + 1]);/* Write the string to the stdout */
+	return (count);
+}
+
+/**
+ * _printf - Custom printf function.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed (excluding the null byte).
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i, count = 0;
+
+	va_start(args, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					count += _putchar(va_arg(args, int));
+					break;
+				case 's':
+					count += write(1, va_arg(args, char *), count);
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+				case 'd':
+				case 'i':
+					count += _printint(va_arg(args, int));
+					break;
+				default:
+					_putchar('%');
+					_putchar(format[i]);
+					count += 2;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+	}
+
+	va_end(args);
+
 	return (count);
 }
 
@@ -86,4 +149,3 @@ int _printbin(int num)
 	free(binary);
 	return (count);
 }
-
